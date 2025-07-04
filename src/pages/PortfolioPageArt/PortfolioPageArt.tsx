328586315463982da2168fd567478a26ab1a9a7c
rouenemedina <rouenemedina@gmail.com>
2025-07-04 08:13:56 -0600

    replaced some assets

--- src/pages/PortfolioPageArt/PortfolioPageArt.tsx
+++ src/pages/PortfolioPageArt/PortfolioPageArt.tsx
@@ -3,9 +3,11 @@ import Header from "../../components/Header/Header";
 import GalleryMarquee from "../../components/GalleryMarquee/GalleryMarquee";
 function PortfolioPageArt() {
   const images = [
-    { src: "/src/assets/images/ArtGallery/art1.png", alt: "art1" },
-    { src: "/src/assets/images/ArtGallery/art2.png", alt: "art2" },
-    { src: "/src/assets/images/ArtGallery/art3.png", alt: "art3" },
+    { src: "/src/assets/images/ArtGallery/art1.jpg", alt: "art1" },
+    { src: "/src/assets/images/ArtGallery/art2.jpg", alt: "art2" },
+    { src: "/src/assets/images/ArtGallery/art3.jpg", alt: "art3" },
+    { src: "/src/assets/images/ArtGallery/art4.jpg", alt: "art4" },
+    { src: "/src/assets/images/ArtGallery/art5.png", alt: "art5" },
   ];
 
   return (
