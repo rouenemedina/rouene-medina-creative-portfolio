85a365d62ac29d8305e525abc63fa903d78ba8a6
rouenemedina <rouenemedina@gmail.com>
2025-07-03 21:13:31 -0600

    built the photography portfolio page and content with styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AboutContactPage from "./pages/AboutContactPage/AboutContactPage";
 import PortfolioPage from "./pages/PortfolioPage/PortfolioPage";
+import PortfolioPhotography from "./pages/PortfolioPhotography/PortfolioPhotography";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/connect" element={<AboutContactPage />}></Route>
       <Route path="/portfolio" element={<PortfolioPage />}></Route>
+      <Route path="/portfolio/photography" element={<PortfolioPhotography />} ></Route>
     </Routes>
     </BrowserRouter>
     </>
---
+++ src/pages/PortfolioPhotography/PortfolioPhotography.tsx
@@ -0,0 +1,16 @@
+import "./PortfolioPhotography.scss";
+import Header from "../../components/Header/Header";
+
+function PortfolioPhotography() {
+  return (
+    <>
+      <Header />
+      <main className="portfolioPhotography">
+        <h1 className="portfolioPhotography__title">Photography Portfolio</h1>
+        <h2 className="portfolioPhotography__subtitle">COMING SOON</h2>
+      </main>
+    </>
+  );
+}
+
+export default PortfolioPhotography;
